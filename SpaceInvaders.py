3771473db91b9633c8a46d7c32b13c31555fe526
Tanner <tanner.drayton01@gmail.com>
2019-07-24 09:39:08 -0500

    Changed player image to spaceship

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -2,6 +2,9 @@
 # Choose an interpreter that works
 import pygame
 
+# Media files
+player_image = pygame.image.load('si-player.gif')
+
 pygame.init()
 clock = pygame.time.Clock()
 game_display = pygame.display.set_mode((400, 600))
@@ -9,25 +12,32 @@ score_font = pygame.font.SysFont('Arial', 22, True)
 title_font = pygame.font.SysFont('Arial', 26, True)
 pygame.display.set_caption('SPACE INVADERS!')
 
+x_coordinate = 200
+y_coordinate = 575
+
 
 def handle_events():
+    global x_coordinate, y_coordinate, is_playing
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            snake.is_alive = False
+            is_playing = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                snake.set_direction_left()
+                x_coordinate -= 10
             elif event.key == pygame.K_RIGHT:
-                snake.set_direction_right()
+                x_coordinate += 10
 
 
 # Main Game Loop
 is_playing = True
 while is_playing:
+
+    handle_events()
+
     game_display.blit(game_display, (0, 0))
 
     game_display.fill((0, 0, 0))
-    pygame.draw.rect(game_display, (0, 255, 0), pygame.Rect(50, 50, 20, 20))
+    game_display.blit(player_image, (x_coordinate, y_coordinate))
     #score_text = score_font.render(str(snake.score), False, WHITE)
     #game_display.blit(score_text, (0,0))
 
@@ -36,3 +46,4 @@ while is_playing:
     clock.tick(30)
 
 pygame.display.quit()
+pygame.quit
